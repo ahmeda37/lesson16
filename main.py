07416862967ac1d128e2d2d58ccde0c8a3ef08d4
Abdurahman Ahmed <abdahmed@live.ca>
2019-11-04 19:37:35 -0600

    lots of changes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
+import datetime
 
-app = Flask()
-
-
+app = Flask(__name__)
+year = datetime.datetime.now().year
+name = "Abdul Ahmed"
+cities = ["Edmonton", "Winnipeg", "Toronto"]
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",year=year,name=name)
+
+
+@app.route('/about-me')
+def about():
+    return render_template("about.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
